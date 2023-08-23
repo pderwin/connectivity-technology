28449b247e6822fd5989a03806efb5b9cec3ca66
Phil Erwin <phil.erwin@lexmark.com>
2023-08-23 12:17:32 -0400

    ct_main: start several threads

--- src/ct_main.c
+++ src/ct_main.c
@@ -2,13 +2,25 @@
 #include <zephyr/init.h>
 #include "accel.h"
 
+void bt_thread_start(void);
+void led_thread_start(void);
+void semtracker_thread_start (void);
+void wifi_thread_start (void);
+
 int ct_main(const struct device *device)
 {
    (void) device;
 
    printk("%s: initializing...\n", __func__);
 
-   accel_init();
+   bt_thread_start();
+   led_thread_start();
+
+   semtracker_thread_start();
+
+//   accel_init();
+
+   wifi_thread_start();
 
    printk("%s: initialization complete\n", __func__);
 
