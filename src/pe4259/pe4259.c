08d05689c0fc6ec324363e1456e91a29b6aaa0bd
Phil Erwin <pderwin2003@yahoo.com>
2024-04-10 15:53:44 -0400

    pe4259: rm traces

--- src/pe4259/pe4259.c
+++ src/pe4259/pe4259.c
@@ -1,6 +1,5 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/trace.h>
 #include <zephyr/devicetree.h>
 #include "pe4259.h"
 
@@ -15,19 +14,17 @@ void pe4259_select(pe4259_select_e select)
 
    switch(select) {
        case PE4259_SELECT_RF1:
-          ctrl_val = 1;
-          vdd_val  = 0;
-          break;
+	  ctrl_val = 1;
+	  vdd_val  = 0;
+	  break;
        case PE4259_SELECT_RF2:
-          ctrl_val = 0;
-          vdd_val  = 1;
-          break;
+	  ctrl_val = 0;
+	  vdd_val  = 1;
+	  break;
        default:
-          printk("%s: Error -- invalid selection: %x from %p \n", __func__, select,__builtin_return_address(0) );
-          return;
+	  printk("%s: Error -- invalid selection: %x from %p \n", __func__, select,__builtin_return_address(0) );
+	  return;
    }
-
-   TRACE2(TAG_PE4259_SELECT, ctrl_val, vdd_val);
 
    gpio_pin_set_dt(&ctrl, ctrl_val);
    gpio_pin_set_dt(&vdd,  vdd_val);
@@ -36,6 +33,6 @@ void pe4259_select(pe4259_select_e select)
 
 void pe4259_init (void)
 {
-    gpio_pin_configure_dt(&ctrl, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&vdd,  GPIO_OUTPUT_INACTIVE);
+   gpio_pin_configure_dt(&ctrl, GPIO_OUTPUT_INACTIVE);
+   gpio_pin_configure_dt(&vdd,  GPIO_OUTPUT_INACTIVE);
 }
